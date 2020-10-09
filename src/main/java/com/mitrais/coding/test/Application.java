00d34d0b6659c26e608fc4d021165b6ae182f37b
hendrakrn23 <warcraft230493>
2020-10-09 08:35:51 +0700

    test commit

--- src/main/java/com/mitrais/coding/test/Application.java
+++ src/main/java/com/mitrais/coding/test/Application.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Import;
 @SpringBootApplication
 @Import({SpringConfig.class})
 public class Application {
-
+	//test commit
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
